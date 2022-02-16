3205140f9796b6d0a13fd7b7ac66e6221502c279
mrm <cmpl.error@gmail.com>
2022-02-17 07:43:17 +0900

    guest: gicv3

---
+++ guest/gicv3.h
@@ -0,0 +1,7 @@
+#ifndef MVMM_GUEST_GICV3_H
+#define MVMM_GUEST_GICV3_H
+
+void gicv3_init(void);
+void gicv3_init_percpu(void);
+
+#endif
--- guest/hello.c
+++ guest/hello.c
@@ -1,10 +1,13 @@
 #include "uart.h"
+#include "gicv3.h"
 
 __attribute__((aligned(16))) char _stack[4096];
 
 int main(void) {
   uart_init();
   uart_puts("hello, guest world!\n");
+  gicv3_init();
+  gicv3_init_percpu();
 
   return 0x199999;
 }
