f697a0566a4fb64dd53984593c4e944e919ac7ac
mrm <cmpl.error@gmail.com>
2022-04-25 03:40:23 +0900

    pcie_scan_dev

--- include/pci.h
+++ include/pci.h
@@ -5,7 +5,35 @@
 #include "types.h"
 
 struct pci_config {
-  ;
+  u16 vendor_id;
+  u16 device_id;
+  u16 command;
+  u16 status;
+  u8 revision_id;
+  u8 prog_if;
+  u8 subclass;
+  u8 class_code;
+  u8 cache_line_sz;
+  u8 latency_timer;
+  u8 header_type;
+  u8 bist;
+  u32 bar0;
+  u32 bar1;
+  u32 bar2;
+  u32 bar3;
+  u32 bar4;
+  u32 bar5;
+  u32 cardbus_cis_ptr;
+  u16 subsystem_vendor_id;
+  u16 subsystem_id;
+  u32 exrom_base;
+  u8 capabilities_ptr;
+  u8 res0[3];
+  u32 res1;
+  u8 intr_line;
+  u8 intr_pin;
+  u8 min_grant;
+  u8 max_latency;
 };
 
 #endif
--- src/pci.c
+++ src/pci.c
@@ -2,9 +2,29 @@
 #include "pci.h"
 #include "log.h"
 
-#define R(reg)  (volatile u32 *)(PCIE_ECAM_BASE+reg)
+struct pci_config_space {
+  u8 raw[256][32][8][4096];
+};
+
+static void pcie_scan_dev() {
+  struct pci_config *cfg;
+  struct pci_config_space *space = (struct pci_config_space *)PCIE_ECAM_BASE;
+
+  for(int bc = 0; bc < 256; bc++)
+    for(int dc = 0; dc < 32; dc++)
+      for(int fc = 0; fc < 8; fc++) {
+        cfg = (struct pci_config *)&space->raw[bc][dc][fc];
+        if(cfg->vendor_id == 0xffff)
+          continue;
+
+        vmm_log("pcie find: %d\n", cfg->vendor_id);
+
+        if(cfg->vendor_id == 6900) {
+          vmm_log("\tvirtio %d sub:%d\n", cfg->device_id, cfg->subsystem_id);
+        }
+      }
+}
 
 void pcie_init() {
-  u32 c = *R(0);
-  vmm_log("pcie %c\n", c);
+  pcie_scan_dev();
 }
